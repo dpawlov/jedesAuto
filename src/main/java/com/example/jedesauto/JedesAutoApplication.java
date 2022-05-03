d11d84fd36c555aafaf7598838dc5467178d2cca
dpawlov <dimitarpawlovvv@gmail.com>
2022-05-03 19:20:33 +0300

    commit

---
+++ src/main/java/com/example/jedesauto/JedesAutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jedesauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedesAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JedesAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jedesauto/JedesAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jedesauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JedesAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
